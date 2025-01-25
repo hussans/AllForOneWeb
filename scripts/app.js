c1d36c521fcee4cac6d8a35da30b91b0090cc23d
hussans <uwnhasan@gmail.com>
2025-01-25 14:30:00 -0800

    Main Menu Page & Say Hello Page

--- scripts/app.js
+++ scripts/app.js
@@ -11,9 +11,31 @@ if (currentPage === '/pages/index.html') {
 
 /* Homepage Menu Button */
 
-const menuBtn = document.getElementById('menuBtn');
+const toMenuBtn = document.getElementById('toMenuBtn');
 
-menuBtn.addEventListener('click', (e) => {
-    window.location.href = '/pages/menu.html'
-});
+if (toMenuBtn) { 
+    toMenuBtn.addEventListener('click', () => {
+        window.location.href = '/pages/menu.html';
+    });
+}
 
+/* Main Menu Buttons */
+
+const sayHelloBtn = document.getElementById('sayHelloBtn');
+const askQuestionBtn = document.getElementById('askQuestionBtn');
+const addNumbersBtn = document.getElementById('addNumbersBtn');
+const madlibBtn = document.getElementById('madlibBtn');
+const oddEvenBtn = document.getElementById('oddEvenBtn');
+const reverseItBtn = document.getElementById('reverseItBtn');
+const reverseNumbersBtn = document.getElementById('reverseNumbersBtn');
+const magicEightBallBtn = document.getElementById('magicEightBallBtn');
+const guessItBtn = document.getElementById('guessItBtn');
+const restaurantPickerBtn = document.getElementById('restaurantPickerBtn');
+
+/* Say Hello Button */
+
+if (sayHelloBtn) { // Check if the element exists
+    sayHelloBtn.addEventListener('click', () => {
+        window.location.href = '/pages/sayhello.html';
+    });
+}
